5931e663f1b806921755b19a269e72f8235c129d
SachinMattihal <sachinmattihal@gmail.com>
2023-03-06 11:30:55 +0530

    submitted

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './components/Nav';
+import "./Global.css"
+const App = () => {
+  return (
+   <>
+   <Nav/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <>
+    <ul>
+        <a href="Home"><li>Home</li></a>
+        <a href="LogIn"><li>LogIn</li></a>
+        <a href="SignUp"><li>SignUp</li></a>
+        <a href="About"><li>About</li></a>
+    </ul>
+    </>
+  )
+}
+
+export default Menu
